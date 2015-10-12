41f31be31dbacfb6ed206c937785fbfff0536be4
asn <yaalsn@gmail.com>
2015-10-12 19:51:44 +0800

    increase correct rate

--- ican.py
+++ ican.py
@@ -8,6 +8,22 @@ import IPGen
 from threading import Thread
 import cStringIO
 from color import tobw
+from collect import to10
+
+d0 = '00011000001111000110011011000011110000111100001111000011011001100011110000011000'
+d1 = '00011000001110000111100000011000000110000001100000011000000110000001100001111110'
+d2 = '00111100011001101100001100000011000001100000110000011000001100000110000011111111'
+d3 = '01111100110001100000001100000110000111000000011000000011000000111100011001111100'
+d4 = '00000110000011100001111000110110011001101100011011111111000001100000011000000110'
+d5 = '11111110110000001100000011011100111001100000001100000011110000110110011000111100'
+d6 = '00111100011001101100001011000000110111001110011011000011110000110110011000111100'
+d7 = '11111111000000110000001100000110000011000001100000110000011000001100000011000000'
+d8 = '00111100011001101100001101100110001111000110011011000011110000110110011000111100'
+d9 = '00111100011001101100001111000011011001110011101100000011010000110110011000111100'
+
+fuck_list ={d0:0,d1:1,d2:2,d3:3,d4:4,d5:5,d6 :6,d7:7,d8:8,d9:9}
+
+wired = '00000000000000000000000000000000000000000000000000000000000000000000000000000000'
 
 def vote():
 	s = requests.Session()
@@ -23,28 +39,19 @@ def vote():
 			continue
 		pic = cStringIO.StringIO(r.content)
 		im = tobw(Image.open(pic))
-		#im.show()
-		#im1 = im.crop((5,6,23,18))
-		#im2 = im.crop((45,6,63,18))
-		#im = Image.open(pic)
-		#im = ImageEnhance.Contrast(im.convert('L')).enhance(20)
-		#ps = r.headers['set-cookie'].split(';')[0]
-		#with open('vcode.png', 'wb') as pic:
-			#pic.write(r.content)
-		try:
-			im = pytesseract.image_to_string(im)
-			#im1 = pytesseract.image_to_string(im1)
-			#im2 = pytesseract.image_to_string(im2)
-		except:
-			continue
-		im = im.replace(' ', '')
-		number = re.findall(r"[0-9]*",im)
+		key1 = to10(im,5,7,12,16)
+		key2 = to10(im,14,7,21,16)
+		key3 = to10(im,45,7,52,16)
+		key4 = to10(im,54,7,61,16)
 		sumn = 0
-		for i in number:
-			if i != '':
-				sumn = sumn + int(i)
-		if sumn == 0:
-			continue
+		if key2!=wired and key4!= wired:
+			sumn = fuck_list[key1] *10 + fuck_list[key2] + fuck_list[key3] *10 + fuck_list[key4]
+		if key2 == wired and key4 == wired:
+			sumn = fuck_list[key1] + fuck_list[key3]
+		if key2 == wired and key4 != wired:
+			sumn = fuck_list[key1] + fuck_list[key3]*10 + fuck_list[key4]
+		if key2 != wired and key4 == wired:
+			sumn = fuck_list[key1]*10 + fuck_list[key2] + fuck_list[key3]
 		ip=IPGen.IPGen().ip()
 		headers = {'Host': 'ican.cdstm.cn',
 		'Connection':'keep-alive',
